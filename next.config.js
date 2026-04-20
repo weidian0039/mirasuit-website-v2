0b30db24fdcf8e48677ad580711ed9e056e9e11d
weidian0039 <weidian0039@gmail.com>
2026-04-20 12:11:24 +0200

    Fix basePath for GitHub Pages subdirectory deploy

--- next.config.js
+++ next.config.js
@@ -3,5 +3,7 @@ const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
   trailingSlash: true,
+  basePath: '/mirasuit-website-v2',
+  assetPrefix: '/mirasuit-website-v2',
 }
 module.exports = nextConfig
